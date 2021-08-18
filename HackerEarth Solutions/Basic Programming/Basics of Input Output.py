a76cf3723f2382814adbcfaa25e0af2d5ca877c4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-08-19 01:00:08 +0530

    Basics of Input Output

---
+++ HackerEarth Solutions/Basic Programming/Basics of Input Output.py
@@ -0,0 +1,14 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+n = int(input())
+s = str(input())
+print(n * 2)
+print(s)
